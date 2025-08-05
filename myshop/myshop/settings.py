864240e2368d20e8633ce65b0c3b4c40956b0d32
Aakashraz <aakashraz@outlook.com>
2025-08-05 23:37:48 +0545

    trying to import 1500 sample data, also generate_csv.py added to generate sample data

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -210,3 +210,8 @@ STRIPE_PUBLISHABLE_KEY = config('STRIPE_PUBLISHABLE_KEY')
 STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY')
 STRIPE_API_VERSION= '2024-04-10'
 STRIPE_WEBHOOK_SECRET = config('STRIPE_WEBHOOK_SECRET')
+
+
+# Increase the max number of POST fields to allow for large admin actions
+# The default is 1000
+DATA_UPLOAD_MAX_NUMBER_FIELDS = 3200
--- myshop/orders/resources.py
+++ myshop/orders/resources.py
@@ -1,6 +1,9 @@
 from import_export import resources
 from .models import Order
+import logging
 
+
+logger = logging.getLogger(__name__)
 
 class OrderResource(resources.ModelResource):
     class Meta:
@@ -9,3 +12,20 @@ class OrderResource(resources.ModelResource):
                   'city', 'paid', 'created', 'updated', 'stripe_id')
         export_order = ('id', 'first_name', 'last_name', 'email', 'address', 'postal_code',
                         'city', 'paid', 'created', 'updated', 'stripe_id')
+        import_id_fields = ('email',)   # Optional unique identifier
+
+
+        def before_import_row(self, row, row_number=None, **kwargs):
+            logger.info(f"Processing row {row_number}: {row.get('email', 'N/A')}")
+            # Alternatively, print directly for immediate output
+            print(f"Processing row {row_number}: {row.get('email', 'N/A')}")
+
+
+        def import_row(self, row, instance, **kwargs):
+            # Process each row (optional additional logging)
+            if instance:
+                logger.info(f"Updating order for email: {row.get('email', 'N/A')}")
+            else:
+                logger.info(f"Creating order for email: {row.get('email', 'N/A')}")
+
+            return super().import_row(row, instance, **kwargs)
