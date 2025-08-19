5348857d44729bc601d4dfc84bb5b131dec85b86
Aakashraz <aakashraz@outlook.com>
2025-08-20 00:28:49 +0545

    code to initialize celery_patch moved from __init__.py to apps.py

--- myshop/orders/__init__.py
+++
@@ -1,5 +0,0 @@
-try:
-    from . import celery_patch
-    print("Celery patch loaded successfully")
-except ImportError as e:
-    print(f"Failed to load celery patch: {e}")
--- myshop/orders/apps.py
+++ myshop/orders/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class OrdersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'orders'
+
+    def ready(self):
+        try:
+            from . import celery_patch
+            print('Celery patched loaded successfully')
+        except ImportError as e:
+            print(f"Celery patch failed to load: {e}")
