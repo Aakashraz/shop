8b05b8cb0db8efc47b965b2b14b63df71732bc0f
Aakashraz <aakashraz@outlook.com>
2025-09-06 22:50:11 +0545

    minor comment(explanation) added into the payment_completed() method

--- myshop/payment/tasks.py
+++ myshop/payment/tasks.py
@@ -21,14 +21,22 @@ def payment_completed(order_id):
     email = EmailMessage(
         subject, message, 'admin@myshop.com', [order.email]
     )
+
     # generate PDF
     html = render_to_string('orders/order/pdf.html', {'order': order})
-    out = BytesIO()
+    out = BytesIO()     # Creates an in-memory buffer (like a virtual file in RAM).
     stylesheets = [weasyprint.CSS(finders.find('css/pdf.css'))]
+    # WeasyPrint takes the rendered HTML + CSS and writes the raw binary PDF data into that buffer.
     weasyprint.HTML(string=html).write_pdf(out, stylesheets=stylesheets)
-    # attach PDF file
+    # Result: 'out' now holds the PDF content in bytes (not readable text)
+
+    # attach a PDF file
     email.attach(
         f'order_{order.id}.pdf', out.getvalue(), 'application/pdf'
     )
+    # out.getvalue() extracts the binary data from the memory buffer.
+    # 'application/pdf' = MIME type, telling the receiver's email client/browser:
+    # "This is a PDF, open with a PDF viewer".
+
     # send e-mail
     email.send()
