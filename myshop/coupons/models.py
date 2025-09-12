6b9e863476187fcb373575f9b70d130ecdc9be9b
Aakashraz <aakashraz@outlook.com>
2025-09-12 23:20:20 +0545

    adding Coupon model to admin site

--- myshop/coupons/models.py
+++ myshop/coupons/models.py
@@ -6,6 +6,10 @@ class Coupon(models.Model):
     code= models.CharField(max_length=50, unique=True)
     valid_from = models.DateTimeField()
     valid_to = models.DateTimeField()
+    # MinValueValidator(0); The discount cannot be less than 0.
+    # MaxValueValidator(100); The discount cannot be more than 100.
+    # These validators run before Django saves the object to the database (and also when you run form validation).
+    # If the value is outside the allowed range, Django will raise a ValidationError.
     discount = models.IntegerField(
         validators=[MinValueValidator(0), MaxValueValidator(100)],
         help_text='Percentage value (0 to 100)'
